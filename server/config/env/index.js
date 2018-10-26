ffe46c0a75f518abfb7963e0f9e19555dade7355
Esli Gonzalez <eslid@gmail.com>
2018-10-26 14:30:46 -0600

    ultimos cambios

--- server/config/env/index.js
+++ server/config/env/index.js
@@ -2,7 +2,7 @@
 
 const env = process.env.NODE_ENV || 'development'
 //const config = require(`./${env}`) // eslint-disable-line import/no-dynamic-require
-const prod = process.env.NODE_ENV || 'producction'
+const prod = process.env.NODE_ENV || 'production'
 const config = require(`./${prod}`)
 const defaults = {
   port: process.env.PORT || 5050,
